48a767254639ae85f2042426b804297b6d0de401
kbarrere <killian.barrere@free.fr>
2019-03-06 11:51:02 +0100

    added results analysis and script to create a csv with MAP, #Rel_words, AP and MAP - 2

---
+++ results-anaysis/create_KWS_csv.py
@@ -0,0 +1,54 @@
+import csv
+import argparse
+
+
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Create a csv file from results of KWS in a text format')
+parser.add_argument('kws_result', help='file containing results of KWS in a text format')
+parser.add_argument('output_csv', help='outputted csv file')
+
+args = parser.parse_args()
+
+
+with open(args.output_csv, 'wb') as csvfile:
+	filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	
+	# Title
+	cols_name=['MAP', '#Rel-Wrds', 'AP', 'RP', 'F1max', 'RCmax', 'PRres', 'CERmin', 'CER(>1.0)']
+	cols_ind={'MAP':0, '#Rel-Wrds':1, 'AP':2, 'RP':3, 'F1max':4, 'RCmax':5, 'PRres':6, 'CERmin':7, 'CER(>1.0)':8}
+	n_score = len(cols_name)
+	filewriter.writerow(cols_name)
+	
+	with open(args.kws_result, 'r') as kws_result:
+		
+		scores = [''] * n_score
+		
+		for line in kws_result:
+			line = line[:-1]
+			line_splitted = line.split('=')
+			metric = line_splitted[0]
+			
+			if 'MAP' in metric:
+				line_splitted_2 = line_splitted[1].split(' ')
+				map_score = line_splitted_2[1]
+				scores[cols_ind['MAP']] = map_score
+				
+				line_splitted_3 = line_splitted[2].split(' ')
+				nbr_rel_words = line_splitted_3[1]
+				scores[cols_ind['#Rel-Wrds']] = nbr_rel_words
+			
+			if 'AP' in metric and 'MAP' not in metric:
+				line_splitted_2 = line_splitted[1].split(' ')
+				ap_score = line_splitted_2[1]
+				scores[cols_ind['AP']] = ap_score
+			
+			if 'RP' in metric:
+				line_splitted_2 = line_splitted[1].split(' ')
+				rp_score = line_splitted_2[1]
+				scores[cols_ind['AP']] = rp_score
+		
+		filewriter.writerow(scores)
+				
+				
+			

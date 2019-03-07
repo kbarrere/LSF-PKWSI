c78e6d6666ae3b9a239331e3d3222c12fcbf9572
kbarrere <killian.barrere@free.fr>
2019-03-07 09:53:56 +0100

    add script to print rp-curve with matplotlib

---
+++ results-anaysis/print_rp_curve.py
@@ -0,0 +1,32 @@
+import argparse
+import matplotlib.pyplot as plt
+
+
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Create a csv file to plot curves from results of KWS in a text format')
+parser.add_argument('kws_result', help='file containing the information')
+
+args = parser.parse_args()
+
+
+with open(args.kws_result, 'r') as kws_result:
+	
+	precision = []
+	recall = []
+	
+	for line in kws_result:
+		if len(line) > 0 and line[0] != '#':
+			row = []
+			
+			line = line[:-1]
+			line_split = line.split(' ')
+			for score in line_split:
+				if len(score) > 0:
+					row.append(score)
+			
+			precision.append(row[0])
+			recall.append(row[1])
+
+plt.plot(recall, precision, label='rp-curve')
+plt.show()

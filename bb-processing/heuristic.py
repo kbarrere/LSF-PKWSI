e10f40f238095f539b993aa8f07f2aa5da3f2042
kbarrere <killian.barrere@free.fr>
2019-03-25 13:33:01 +0100

    extract and store bounding boxes

--- bb-processing/heuristic.py
+++ bb-processing/heuristic.py
@@ -1,15 +1,97 @@
 import argparse
+from xmlPAGE import *
+
 
 
 if __name__ == '__main__':
 	
 	# Parse arguments
 	parser = argparse.ArgumentParser(description='Print Bounding boxes for the given index file and image')
+	parser.add_argument('pages_path', nargs='+', help='path to page xml file associated to the image and the lines')
 	parser.add_argument('index_path', help='path to index file containing keywords, score and positioninng in the lines')
 	
 	args = parser.parse_args()
 
-with open(args.index_path, 'r') as index_file:
-	for line in index_file:
-		line = line[:-1]
-		print(line)
+
+
+class BB:
+	def __init__(self, xmin, ymin, xmax, ymax, score):
+		self.xmin = xmin
+		self.ymin = ymin
+		self.xmax = xmax
+		self.ymax = ymax
+		self.score = score
+		self.score = score
+	
+	def get_coords(self):
+		return self.xmin, self.ymin, self.xmax, self.ymax
+	
+	def get_score(self):
+		return self.score
+
+
+
+# Reading the index file
+index_file = open(args.index_path, 'r')
+index_dict = {}
+for line in index_file:
+	line = line[:-1]
+	lineID, keyword, score_str, start_frame_str, end_frame_str, total_frame_str = line.split(' ')
+	pageID, lineID = lineID.split('.')
+	score = float(score_str)
+	start_frame = int(start_frame_str)
+	end_frame = int(end_frame_str)
+	total_frame = int(total_frame_str)
+	
+	if pageID not in index_dict:
+		index_dict[pageID] = {}
+	if lineID not in index_dict[pageID]:
+		index_dict[pageID][lineID] = []
+	index_dict[pageID][lineID].append([keyword, score, start_frame, end_frame, total_frame])
+	
+index_file.close()
+
+
+bbxs_dict = {}
+
+# Loop over all page XML files
+for page_path in args.pages_path:
+	# Get the pageID from the path
+	pageID = page_path.split('/')[-1].split('.')[0]
+	if pageID in index_dict:
+		# Open Page XML
+		page = pageData(page_path)
+		page.parse()
+		textline_elements = page.get_region('TextLine')
+
+		for textline_element in textline_elements:
+			lineID = page.get_id(textline_element)
+			
+			line_coords = page.get_coords(textline_element)
+			line_xmin, line_ymin = line_coords[0]
+			line_xmax, line_ymax = line_coords[2]
+			line_width = line_xmax - line_xmin
+			# ~ line_height = line_ymax - line_ymin
+			
+			if lineID in index_dict[pageID]:
+				for detected in index_dict[pageID][lineID]:
+					keyword, score, start_frame, end_frame, total_frame = detected
+					
+					# Create a bounding box
+					xmin = int(line_xmin + float(start_frame)/float(total_frame)*line_width)
+					xmax = int(line_xmin + float(end_frame)/float(total_frame)*line_width)
+					ymin = line_ymin
+					ymax = line_ymax
+					bb = BB(xmin, ymin, xmax, ymax, score)
+					
+					if pageID not in bbxs_dict:
+						bbxs_dict[pageID] = {}
+					if keyword not in bbxs_dict[pageID]:
+						bbxs_dict[pageID][keyword] = []
+					
+					bbxs_dict[pageID][keyword].append(bb)
+					
+	else:
+		print("Could not find any page indexed with pageID: " + pageID)
+
+print(bbxs_dict['RS_Aicha_vorm_Wald_031_0187']['ANNA'])

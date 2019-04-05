3f5299d8d2629468d835eacb6eecca7f21d0c40f
kbarrere <killian.barrere@free.fr>
2019-04-05 12:26:04 +0200

    added every part for the reading and make the script executable

---
+++ bb-processing/merge_bbxs.py
@@ -0,0 +1,178 @@
+import argparse
+from xmlPAGE import *
+
+if __name__ == '__main__':
+	
+	# Parse arguments
+	parser = argparse.ArgumentParser(description='Merge Bounding boxes')
+	parser.add_argument('index_path', help='path to index file containing keywords, score and positioninng in the lines (in the format pageID.LineID keyword score startframe endframe totalframe')
+	parser.add_argument('pages_path', nargs='+', help='path to page xml file associated with the index')
+	parser.add_argument('output_index', help='path to the outputted index. in the format pageID keyword  proba xmin ymin xmax ymax')
+	
+	args = parser.parse_args()
+
+class BB:
+	def __init__(self, xmin, ymin, xmax, ymax, score):
+		self.xmin = xmin
+		self.ymin = ymin
+		self.xmax = xmax
+		self.ymax = ymax
+		self.score = score
+		self.score = score
+	
+	def get_coords(self):
+		return self.xmin, self.ymin, self.xmax, self.ymax
+	
+	def get_score(self):
+		return self.score
+	
+	def __repr__(self):
+		return "BB("+self.__str__()+")"
+	
+	def __str__(self):
+		return "xmin="+str(self.xmin)+" ymin="+str(self.ymin)+" xmax="+str(self.xmax)+" ymax="+str(self.ymax)+" score="+str(self.score)
+
+def bb_equal(bb1, bb2):
+	xmin1, ymin1, xmax1, ymax1 = bb1.get_coords()
+	xmin2, ymin2, xmax2, ymax2 = bb2.get_coords()
+	score1 = bb1.get_score()
+	score2 = bb2.get_score()
+	
+	return (xmin1 == xmin2 and ymin1 == ymin2 and xmax1 == xmax2 and ymax1 == ymax2 and score1 == score2)
+
+def is_in_range(xmin, xmax, x):
+	return (xmin <= x and x <= xmax)
+
+def is_intersection_segment(xmin1, xmax1, xmin2, xmax2):
+	return (is_in_range(xmin1, xmax1, xmin2) or is_in_range(xmin1, xmax1, xmax2) or is_in_range(xmin2, xmax2, xmin1) or is_in_range(xmin2, xmax2, xmax1))
+
+def is_intersection_bb(bb1, bb2):
+	xmin1, ymin1, xmax1, ymax1 = bb1.get_coords()
+	xmin2, ymin2, xmax2, ymax2 = bb2.get_coords()
+	
+	return (is_intersection_segment(xmin1, xmax1, xmin2, xmax2) and is_intersection_segment(ymin1, ymax1, ymin2, ymax2))
+
+def overlap_percent(bb1, bb2):
+	xmin1, ymin1, xmax1, ymax1 = bb1.get_coords()
+	xmin2, ymin2, xmax2, ymax2 = bb2.get_coords()
+	
+	xmin = max(xmin1, xmin2)
+	ymin = max(ymin1, ymin2)
+	xmax = min(xmax1, xmax2)
+	ymax = min(ymax1, ymax2)
+	
+	base_area = (xmax1 - xmin1) * (ymax1 - ymin1)
+	area = (xmax - xmin) * (ymax - ymin)
+	
+	percentage = 0.0
+	if base_area > 0.0:
+		percentage = float(area)/float(base_area)
+	
+	return percentage
+
+def new_merge_bb_group(bb_list):
+	total_mass = 0.0
+	total_x = 0.0
+	total_y = 0.0
+	total_w = 0.0
+	total_h = 0.0
+	max_score = 0.0
+	
+	for bb in bb_list:
+		xmin, ymin, xmax, ymax = bb.get_coords()
+		score = bb.get_score()
+		
+		total_mass += score
+		total_x += score * (xmax + xmin) / 2
+		total_y += score * (ymax + ymin) / 2
+		total_w += score*(xmax - xmin)
+		total_h += score*(ymax - ymin)
+		
+		max_score = max(max_score, score)
+	
+	# Temporary solution to avoid division by 0
+	if total_mass == 0.0:
+		return bb_list[0]
+	
+	center_x = total_x / total_mass
+	center_y = total_y / total_mass
+	center_w = total_w / total_mass
+	center_h = total_h / total_mass
+	
+	xmin = center_x - center_w / 2
+	xmax = center_x + center_w / 2
+	ymin = center_y - center_h / 2
+	ymax = center_y + center_h / 2
+	
+	bb = BB(int(xmin), int(ymin), int(xmax), int(ymax), max_score)
+	
+	return bb
+
+
+
+
+# Reading the index file
+index_file = open(args.index_path, 'r')
+index_dict = {}
+for line in index_file:
+	line = line[:-1]
+	lineID, keyword, score_str, start_frame_str, end_frame_str, total_frame_str = line.split(' ')
+	pageID, lineID = lineID.split('.')
+	score = float(score_str)
+	start_frame = int(start_frame_str)
+	end_frame = int(end_frame_str)
+	total_frame = int(total_frame_str)
+	
+	if pageID not in index_dict:
+		index_dict[pageID] = {}
+	if lineID not in index_dict[pageID]:
+		index_dict[pageID][lineID] = []
+	index_dict[pageID][lineID].append([keyword, score, start_frame, end_frame, total_frame])
+	
+index_file.close()
+
+
+bbxs_dict = {}
+
+# Loop over all page XML files
+for page_path in args.pages_path:
+	# Get the pageID from the path
+	pageID = page_path.split('/')[-1].split('.')[0]
+	if pageID in index_dict:
+		# Open Page XML
+		page = pageData(page_path)
+		page.parse()
+		textline_elements = page.get_region('TextLine')
+
+		for textline_element in textline_elements:
+			lineID = page.get_id(textline_element)
+			
+			line_coords = page.get_coords(textline_element)
+			line_xmin, line_ymin = line_coords[0]
+			line_xmax, line_ymax = line_coords[2]
+			line_width = line_xmax - line_xmin
+			# ~ line_height = line_ymax - line_ymin
+			
+			if lineID in index_dict[pageID]:
+				for detected in index_dict[pageID][lineID]:
+					keyword, score, start_frame, end_frame, total_frame = detected
+					
+					# Create a bounding box
+					xmin = int(line_xmin + float(start_frame)/float(total_frame)*line_width)
+					xmax = int(line_xmin + float(end_frame)/float(total_frame)*line_width)
+					ymin = line_ymin
+					ymax = line_ymax
+					bb = BB(xmin, ymin, xmax, ymax, score)
+					
+					if pageID not in bbxs_dict:
+						bbxs_dict[pageID] = {}
+					if keyword not in bbxs_dict[pageID]:
+						bbxs_dict[pageID][keyword] = ([], []) # BB_list, frame_numbers
+					
+					bbxs_dict[pageID][keyword][0].append(bb)
+					bbxs_dict[pageID][keyword][1].append((lineID, start_frame, end_frame, total_frame))
+					
+	else:
+		print("Could not find any page indexed with pageID: " + pageID)
+
+print(bbxs_dict)

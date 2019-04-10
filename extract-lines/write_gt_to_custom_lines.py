5d3fc7d173f6bd6a6a039fc62bc55956d41fdd19
kbarrere <killian.barrere@free.fr>
2019-04-10 12:34:56 +0200

    reading the gt

--- extract-lines/write_gt_to_custom_lines.py
+++ extract-lines/write_gt_to_custom_lines.py
@@ -11,6 +11,69 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-print(args.custom_pages)
-print(args.gt_pages)
-print(args.output_path)
+class BB:
+	def __init__(self, xmin, ymin, xmax, ymax, score):
+		self.xmin = xmin
+		self.ymin = ymin
+		self.xmax = xmax
+		self.ymax = ymax
+		self.score = score
+		self.score = score
+	
+	def get_coords(self):
+		return self.xmin, self.ymin, self.xmax, self.ymax
+	
+	def get_score(self):
+		return self.score
+	
+	def __repr__(self):
+		return "BB("+self.__str__()+")"
+	
+	def __str__(self):
+		return "xmin="+str(self.xmin)+" ymin="+str(self.ymin)+" xmax="+str(self.xmax)+" ymax="+str(self.ymax)+" score="+str(self.score)
+
+def is_in_range(xmin, xmax, x):
+	return (xmin <= x and x <= xmax)
+
+def is_intersection_segment(xmin1, xmax1, xmin2, xmax2):
+	return (is_in_range(xmin1, xmax1, xmin2) or is_in_range(xmin1, xmax1, xmax2) or is_in_range(xmin2, xmax2, xmin1) or is_in_range(xmin2, xmax2, xmax1))
+
+def is_intersection_bb(bb1, bb2):
+	xmin1, ymin1, xmax1, ymax1 = bb1.get_coords()
+	xmin2, ymin2, xmax2, ymax2 = bb2.get_coords()
+	
+	return (is_intersection_segment(xmin1, xmax1, xmin2, xmax2) and is_intersection_segment(ymin1, ymax1, ymin2, ymax2))
+
+def overlap_percent(bb1, bb2):
+	xmin1, ymin1, xmax1, ymax1 = bb1.get_coords()
+	xmin2, ymin2, xmax2, ymax2 = bb2.get_coords()
+	
+	xmin = max(xmin1, xmin2)
+	ymin = max(ymin1, ymin2)
+	xmax = min(xmax1, xmax2)
+	ymax = min(ymax1, ymax2)
+	
+	base_area = (xmax1 - xmin1) * (ymax1 - ymin1)
+	area = (xmax - xmin) * (ymax - ymin)
+	
+	percentage = 0.0
+	if base_area > 0.0:
+		percentage = float(area)/float(base_area)
+	
+	return percentage
+
+
+# Open Page XML
+gt_page = pageData(args.gt_page)
+gt_page.parse()
+textline_elements = gt_page.get_region('TextLine')
+
+for textline_element in textline_elements:
+	line_coords = gt_page.get_coords(textline_element)
+	line_xmin, line_ymin = line_coords[0]
+	line_xmax, line_ymax = line_coords[2]
+	line_width = line_xmax - line_xmin
+	line_height = line_ymax - line_ymin
+	
+	text = gt_page.get_text(textline_element)
+	print(text)

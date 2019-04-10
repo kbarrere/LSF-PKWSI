b339dd30572dbb20f68e01d07fbfcadc31663980
kbarrere <killian.barrere@free.fr>
2019-04-10 12:22:14 +0200

    now only using one image at a time

--- extract-lines/write_gt_to_custom_lines.py
+++ extract-lines/write_gt_to_custom_lines.py
@@ -4,8 +4,13 @@ from xmlPAGE import *
 if __name__ == '__main__':
 	
 	# Parse arguments
-	parser.add_argument('custom_pages', nargs='+', help='path to page xml files associated with the custom line extraction')
-	parser.add_argument('gt_pages', nargs='+', help='path to page xml files associated with the ground truth')
-	parser.add_argument('output_path', nargs='+', help='path where to save the resultinge pages')
+	parser = argparse.ArgumentParser(description='Take the GT and write it inside custom lines')
+	parser.add_argument('custom_page', help='path to page xml file associated with the custom line extraction')
+	parser.add_argument('gt_page', help='path to page xml file associated with the ground truth')
+	parser.add_argument('output_path', help='path where to save the resulting page')
 
 	args = parser.parse_args()
+
+print(args.custom_pages)
+print(args.gt_pages)
+print(args.output_path)

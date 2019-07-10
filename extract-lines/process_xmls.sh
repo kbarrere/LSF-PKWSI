23f9223046e16392c47b9b647486e0ab39eadf7d
kbarrere <killian.barrere@free.fr>
2019-07-10 10:27:51 +0200

    renamed extract-lines to extract-slices

--- extract-lines/process_xmls.sh
+++
@@ -1,9 +0,0 @@
-for customxml in $(ls output/*.xml);
-	do xml=$(echo $customxml | sed 's/output\///');
-	echo $xml;
-	gtxml=$(ls Train/*.xml | grep $xml);
-	echo $gtxml;
-	outputxml="processed-xmls/$xml";
-	echo $outputxml;
-	python write_gt_to_custom_lines.py $customxml $gtxml $outputxml;
-done
